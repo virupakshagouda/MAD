c76e39c0c82bd49276ae7e2dbeec331309b399d6
virupakshagouda <52944329+virupakshagouda@users.noreply.github.com>
2020-03-24 21:08:46 +0530

    Create AlarmReceiver.java

---
+++ lab10/AlarmReceiver.java
@@ -0,0 +1,28 @@
+package com.example.lab10mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+   @Override
+    public void onReceive(Context context, Intent intent) {
+       Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+       Uri alarmUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+       if(alarmUri==null)
+       {
+           alarmUri=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+       }
+       Ringtone ringtone=RingtoneManager.getRingtone(context,alarmUri);
+       ringtone.play();
+
+    }
+}
